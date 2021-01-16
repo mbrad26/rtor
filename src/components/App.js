795d4921df4b5780da659ed8d8f32d3bc6078d9d
mbrad26 <mbrad26@gmail.com>
2021-01-16 11:11:29 +0000

    list

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,23 @@
 import React from 'react';
 
-const title = 'React.js';
+const list = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    }, 
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+  }, 
+];
 
 const welcome = {
   greeting: 'Hey',
@@ -16,6 +33,21 @@ const App = () => {
 
       <label htmlFor='search'>Search</label>
       <input type='text' id='search' />
+
+      {list.map(item => {
+          return(
+            <div key={item.objectID}>
+              <span>
+                <a href={item.url}>{item.title} -</a>
+              </span>
+              <span> {item.author}</span>
+              <span> {item.num_comments}</span>
+              <span> {item.points}</span>
+              {item.title}
+            </div>
+          )
+        } 
+      )}
     </div>
   );
 };
